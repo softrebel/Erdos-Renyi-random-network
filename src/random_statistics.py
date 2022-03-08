4e001f91e0cb66f3d8a21d01b6e23ea637dbbe5d
Mohammadreza Shaghouzi <sh.mohammad66@gmail.com>
2022-03-08 11:16:39 +0330

    add giant component size, k max calculation

---
+++ src/random_statistics.py
@@ -0,0 +1,87 @@
+from scipy.special import lambertw
+from scipy.special import factorial
+import numpy as np
+from intersect import intersection
+import matplotlib.pyplot as plt
+
+def calculate_lambert(x):
+    return (1 + (lambertw((-1 * x * np.power(np.e, -1 * x)))) / x)
+
+def calculate_by_lambert(mean_k):
+    c = np.arange(1.1, 5.0, 0.1)
+    s = calculate_lambert(c)
+
+    plt.plot(c,s)
+    plt.xlabel('c')
+    plt.ylabel('s')
+    plt.savefig('lambert.svg')
+    plt.close()
+    s=calculate_lambert(mean_k).real.astype(np.float32)
+    return s
+
+def calculate_by_curves_intersections(mean_k):
+    x = np.arange(0, 1.1, 0.1)
+    y = 1 - np.power(np.e, -1 * mean_k * x)
+    plt.plot(x, y)
+    plt.plot(x, x, '-r')
+    plt.xlabel('S')
+    plt.ylabel('y')
+    plt.xticks(np.arange(0, 1.1, 0.1))
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    _x, _y = intersection(x, y, x, x)
+    plt.plot(_x, _y, "*k")
+
+    # plt.show()
+    plt.savefig('giant_component_size_curve_intersection.svg')
+    plt.close()
+    return list(_x)
+
+
+def calculate_probability_k_max(k_max,mean_k):
+
+    return np.power(np.e,-1*mean_k)*(np.power(mean_k,k_max+1))/factorial(k_max+1)
+
+
+def calculate_kmax_from_guess(N,mean_k):
+    ideal_max_count=1
+    k_max=np.floor(mean_k)-1
+    difference=float('inf')
+    while difference > 1:
+        k_max += 1
+        p_max=calculate_probability_k_max(k_max,mean_k)
+        k_max_count=p_max*N
+        difference=k_max_count-ideal_max_count
+    return k_max
+
+
+
+def get_statistics_of_random_network(N,mean_k):
+    print('Calculcating Giant Component Size (S)')
+    s_lambert=calculate_by_lambert(mean_k)
+    print("calculate from lambert w function:")
+    print(f"S = {s_lambert}")
+    s_curves_intersections=calculate_by_curves_intersections(mean_k)
+    print("intersections of curves:")
+    for index in range(len(s_curves_intersections)):
+        print(f'S({index}) = {float(s_curves_intersections[index])}')
+
+    print('Calculcating Maximum Degree (K_max)')
+    kmax=calculate_kmax_from_guess(N,mean_k)
+    print(f"k_max = {kmax}")
+
+    return (s_lambert,*s_curves_intersections)
+
+
+
+
+if __name__ == '__main__':
+    mean_k=1.5
+    # mean_k=1000
+    N=1000
+    # N=10**9
+    import sys
+    if(len(sys.argv)>1):
+        mean_k=float(sys.argv[1])
+        N=int(sys.argv[2])
+    out=get_statistics_of_random_network(N,mean_k)
+    print(out)
---
+++ test/testrandomstatistics.py
@@ -0,0 +1,31 @@
+import unittest
+from src.random_statistics import *
+class TestRandomStatistics(unittest.TestCase):
+
+    def test_calculate_s_by_lambert_w_function(self):
+        mean_k=1.5
+        result=0.5828116536140442
+        res=calculate_by_lambert(mean_k)
+        self.assertEqual(res, result)
+
+    def test_calculate_s_by_curve_intersection(self):
+        mean_k=1.5
+        result=0.5807922135825879
+        res=calculate_by_curves_intersections(mean_k)
+        self.assertEqual(res[1], result)
+
+    def test_calculate_k_max_probabilty(self):
+        mean_k=1.5
+        result=0.2510
+        res=calculate_probability_k_max(1,mean_k)
+        self.assertEqual(np.round(res,4), result)
+
+    def test_test_calculate_k_max_from_guess(self):
+        mean_k=5
+        N=1000
+        result=12
+        res = calculate_kmax_from_guess(N,mean_k)
+        self.assertEqual(res, result)
+
+if __name__ == '__main__':
+    unittest.main()
