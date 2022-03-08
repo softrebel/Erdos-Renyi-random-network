486e531104db02a0b353ef635f4aedc2b670f350
Mohammadreza Shaghouzi <sh.mohammad66@gmail.com>
2022-03-08 22:26:23 +0330

    add Mr. RafeieFard's solutions for s & k_max

--- src/random_statistics.py
+++ src/random_statistics.py
@@ -3,15 +3,17 @@ from scipy.special import factorial
 import numpy as np
 from intersect import intersection
 import matplotlib.pyplot as plt
+import math
+
 
 def calculate_lambert(x):
     return (1 + (lambertw((-1 * x * np.power(np.e, -1 * x)))) / x)
 
-def calculate_by_lambert(mean_k):
+def calculate_s_by_lambert(mean_k):
     c = np.arange(1.1, 5.0, 0.1)
     s = calculate_lambert(c)
 
-    plt.plot(c,s)
+    plt.plot(c,s.real)
     plt.xlabel('c')
     plt.ylabel('s')
     plt.savefig('lambert.svg')
@@ -19,7 +21,7 @@ def calculate_by_lambert(mean_k):
     s=calculate_lambert(mean_k).real.astype(np.float32)
     return s
 
-def calculate_by_curves_intersections(mean_k):
+def calculate_s_by_curves_intersections(mean_k):
     x = np.arange(0, 1.1, 0.1)
     y = 1 - np.power(np.e, -1 * mean_k * x)
     plt.plot(x, y)
@@ -55,21 +57,60 @@ def calculate_kmax_from_guess(N,mean_k):
 
 
 
+
+def calculate_s_by_binary_search(mean_k):
+    s = 0.5
+    min_s, max_s = 0, 1
+    dif = s - 1 + math.exp(-mean_k * s)
+    while math.fabs(dif) > 1e-5:
+        if dif > 0:
+            max_s = s
+        else:
+            min_s = s
+        s = (min_s + max_s) / 2
+        dif = s - 1 + math.exp(-mean_k * s)
+
+    return s
+
+
+def calculate_k_max_by_series(N,mean_k):
+    k_max_the = math.floor(mean_k - 1)
+    critical = 1 - 1 / N
+    f_k_max = 0
+    while f_k_max < critical:
+        k_max_the += 1
+        f_k_max = 0
+        for k in range(0, k_max_the + 1):
+            f_k_max = f_k_max + (mean_k ** k) / math.factorial(k)
+        f_k_max = math.exp(-mean_k) * f_k_max
+
+    return k_max_the
+
+
+
 def get_statistics_of_random_network(N,mean_k):
-    print('Calculcating Giant Component Size (S)')
-    s_lambert=calculate_by_lambert(mean_k)
+    print('--- Calculcating Giant Component Size (S) ---')
+    s_lambert=calculate_s_by_lambert(mean_k)
     print("calculate from lambert w function:")
     print(f"S = {s_lambert}")
-    s_curves_intersections=calculate_by_curves_intersections(mean_k)
-    print("intersections of curves:")
+    s_curves_intersections=calculate_s_by_curves_intersections(mean_k)
+    print("calculate from intersections of curves:")
     for index in range(len(s_curves_intersections)):
         print(f'S({index}) = {float(s_curves_intersections[index])}')
+    print("calculate from binary search:")
+    s_binary_search = calculate_s_by_binary_search(mean_k)
+    print(f"S = {s_binary_search}")
+    print('')
+    print('--- Calculcating Maximum Degree (K_max) ---')
+    print("calculate from Approximation:")
+    kmax_approximated=calculate_kmax_from_guess(N,mean_k)
+    print(f"k_max = {kmax_approximated}")
 
-    print('Calculcating Maximum Degree (K_max)')
-    kmax=calculate_kmax_from_guess(N,mean_k)
-    print(f"k_max = {kmax}")
+    print("calculate from Series:")
+    kmax_series=calculate_k_max_by_series(N,mean_k)
+    print(f"k_max = {kmax_series}")
 
-    return (s_lambert,*s_curves_intersections)
+    return (s_lambert,*s_curves_intersections,s_binary_search,kmax_approximated,kmax_series)
 
 
 
@@ -84,4 +125,4 @@ if __name__ == '__main__':
         mean_k=float(sys.argv[1])
         N=int(sys.argv[2])
     out=get_statistics_of_random_network(N,mean_k)
-    print(out)
+    print(f'out = {out}')
--- test/testrandomstatistics.py
+++ test/testrandomstatistics.py
@@ -5,13 +5,13 @@ class TestRandomStatistics(unittest.TestCase):
     def test_calculate_s_by_lambert_w_function(self):
         mean_k=1.5
         result=0.5828116536140442
-        res=calculate_by_lambert(mean_k)
+        res=calculate_s_by_lambert(mean_k)
         self.assertEqual(res, result)
 
     def test_calculate_s_by_curve_intersection(self):
         mean_k=1.5
         result=0.5807922135825879
-        res=calculate_by_curves_intersections(mean_k)
+        res=calculate_s_by_curves_intersections(mean_k)
         self.assertEqual(res[1], result)
 
     def test_calculate_k_max_probabilty(self):
@@ -25,6 +25,19 @@ class TestRandomStatistics(unittest.TestCase):
         N=1000
         result=12
         res = calculate_kmax_from_guess(N,mean_k)
+        self.assertEqual(res, result)
+
+    def test_calculate_k_max_by_series(self):
+        mean_k=5
+        N=1000
+        result=13
+        res = calculate_k_max_by_series(N,mean_k)
+        self.assertEqual(res, result)
+
+    def test_calculate_s_by_binary_search(self):
+        mean_k = 1.5
+        result = 0.58282470703125
+        res = calculate_s_by_binary_search(mean_k)
         self.assertEqual(res, result)
 
 if __name__ == '__main__':
